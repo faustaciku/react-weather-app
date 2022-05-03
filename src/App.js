d39b88d4385c98602efa863c10d8648376f6d663
faustaciku <faustaciku@gmail.com>
2022-05-03 13:39:34 +0100

    Created a heading and added a link to Github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+      <footer>
+        This project was coded by Fausta Ciku and it is{" "}
+        <a href="https://github.com/faustaciku/react-weather-app">
+          {" "}
+          open-sourced.
+        </a>
+      </footer>
     </div>
   );
 }
